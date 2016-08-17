e1fef24003fedd51e6c8809f40fe188c67450330
RAM0N_LUIS <ramonlrodriguez@uchicago.edu>
2016-08-16 20:05:11 -0500

    initial commit

---
+++ src/main/java/bellhop/ProWebServiceApplication.java
@@ -0,0 +1,12 @@
+package bellhop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProWebServiceApplication.class, args);
+	}
+}
