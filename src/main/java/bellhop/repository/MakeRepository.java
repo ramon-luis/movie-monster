e2a967812523d52788e697ca2e18725237c865f5
RAM0N_LUIS <ramonlrodriguez@uchicago.edu>
2016-08-16 20:24:24 -0500

    repository files based on sportsvan example

---
+++ src/main/java/bellhop/repository/MakeRepository.java
@@ -0,0 +1,11 @@
+package bellhop.repository;
+
+import bellhop.domain.Make;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by RAM0N on 8/16/16.
+ */
+public interface MakeRepository extends CrudRepository<Make, Long> {
+
+}
---
+++ src/main/java/bellhop/repository/VanRepository.java
@@ -0,0 +1,10 @@
+package bellhop.repository;
+
+import bellhop.domain.Van;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by RAM0N on 8/16/16.
+ */
+public interface VanRepository extends CrudRepository<Van, Long{
+}
